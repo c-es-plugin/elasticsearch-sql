7551b5c4d8d99242462f172e2fe4d1ca9fd0abab
林雪清 <linxq@csdn.net>
2016-12-13 19:16:06 +0800

    实现SqlParseAnalyzer的单元测试，并因此重构相关代码。

--- src/main/java/org/elasticsearch/plugin/nlpcn/preAnalyzer/AnsjAnalyzer.java
+++ src/main/java/org/elasticsearch/plugin/nlpcn/preAnalyzer/AnsjAnalyzer.java
@@ -1,16 +1,61 @@
 package org.elasticsearch.plugin.nlpcn.preAnalyzer;
 
-import org.elasticsearch.common.component.AbstractComponent;
-import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugin.nlpcn.request.HttpRequester;
+import org.elasticsearch.plugin.nlpcn.request.HttpResponse;
+
+import java.net.URLEncoder;
 
 /**
- * Created by fangbb on 2016-12-6.
+ * Created by fangbb on 2016-12-12.
  */
-public class AnsjAnalyzer extends AbstractComponent {
-    @Inject
-    public AnsjAnalyzer(final Settings settings){
-        super(settings);
-        AnsjElasticConfigurator.init(settings);
+public class Analyzer {
+
+    public static String[] analyzer(String term) throws Exception {
+        //TODO done
+        HttpRequester request = new HttpRequester();
+        HttpResponse response = null;
+        String sourceTerms = "";
+//        http://192.168.25.11:9688/_cat/analyze?text=大数据&analyzer=query_ansj
+        try {
+            //String ip = InetAddress.getLocalHost().getHostAddress();
+            String ip = AnsjElasticConfigurator.ES_IP;
+            String port = AnsjElasticConfigurator.ES_PORT;
+            String midUrl = "/_cat/analyze?analyzer=query_ansj&text=";
+            //String preUrl = "http://" + ip + ":" + port + midUrl;
+            String preUrl = "http://192.168.25.11:9688/_cat/analyze?analyzer=query_ansj&text=";
+            //System.out.println(preUrl);
+            String enTerm = URLEncoder.encode(term, "UTF-8");
+            String url = preUrl + enTerm;
+            //System.out.println(url);
+            response = request.sendGet(url);
+            if (response.getCode() == 200) {
+                if (response != null && response.getContent().length() > 10) {
+                    sourceTerms = response.getContent();
+                }
+            }
+
+        } catch (Exception e) {
+            throw new Exception("There is an error in the word segmentation");
+        }
+        return getTerms(sourceTerms).split(",");
     }
+
+    private static String getTerms(String sourceTerms) {
+        StringBuffer sb = new StringBuffer();
+        String[] lines = sourceTerms.split("\n");
+        int lineLen = lines.length;
+        for (int i = 0; i < lineLen; i++) {
+            String[] terms = lines[i].split("\t");
+            String term = terms[0].trim();
+            int size = terms.length;
+            if (i == 0) {
+                //sb.append("\"").append(term).append("\"");
+                sb.append(term);
+            } else {
+                sb.append(",").append(term);
+            }
+        }
+        return sb.toString();
+    }
+
 }
