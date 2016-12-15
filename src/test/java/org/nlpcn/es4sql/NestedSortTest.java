d977e29a7641a41ab5f76a75922a61f79c7fc4d2
gitxxx <binsandy@qq.com>
2016-12-15 15:33:57 +0800

    add unit test 4 nesting sort

--- src/test/java/org/nlpcn/es4sql/NestedSortTest.java
+++ src/test/java/org/nlpcn/es4sql/NestedSortTest.java
@@ -3,6 +3,7 @@ package org.nlpcn.es4sql;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.plugin.deletebyquery.DeleteByQueryPlugin;
+import org.junit.Assert;
 import org.junit.Test;
 import org.nlpcn.es4sql.query.QueryAction;
 import org.nlpcn.es4sql.query.SqlElasticRequestBuilder;
@@ -17,27 +18,190 @@ import java.net.UnknownHostException;
 public class NestedSortTest {
     @Test
     public void nestedSortTest(){
+            String sql = "select * from test where nested(info,info.name=term(\"java\") and info.name=term(\"python\") or info.name=term(\"Hadoop\")) order by nested(info,sum(info.age),info.name=matchQuery(\"java\") and info.name=term(\"python\") and info.name=\"Hadoop\") desc,score desc";
+            String actual = exec(sql);
+            String expected = "{\n" +
+                    "  \"from\" : 0,\n" +
+                    "  \"size\" : 200,\n" +
+                    "  \"query\" : {\n" +
+                    "    \"bool\" : {\n" +
+                    "      \"must\" : {\n" +
+                    "        \"nested\" : {\n" +
+                    "          \"query\" : {\n" +
+                    "            \"bool\" : {\n" +
+                    "              \"must\" : {\n" +
+                    "                \"bool\" : {\n" +
+                    "                  \"should\" : [ {\n" +
+                    "                    \"bool\" : {\n" +
+                    "                      \"must\" : [ {\n" +
+                    "                        \"term\" : {\n" +
+                    "                          \"info.name\" : \"java\"\n" +
+                    "                        }\n" +
+                    "                      }, {\n" +
+                    "                        \"term\" : {\n" +
+                    "                          \"info.name\" : \"python\"\n" +
+                    "                        }\n" +
+                    "                      } ]\n" +
+                    "                    }\n" +
+                    "                  }, {\n" +
+                    "                    \"term\" : {\n" +
+                    "                      \"info.name\" : \"Hadoop\"\n" +
+                    "                    }\n" +
+                    "                  } ]\n" +
+                    "                }\n" +
+                    "              }\n" +
+                    "            }\n" +
+                    "          },\n" +
+                    "          \"path\" : \"info\"\n" +
+                    "        }\n" +
+                    "      }\n" +
+                    "    }\n" +
+                    "  },\n" +
+                    "  \"sort\" : [ {\n" +
+                    "    \"info.age\" : {\n" +
+                    "      \"order\" : \"desc\",\n" +
+                    "      \"missing\" : \"_last\",\n" +
+                    "      \"mode\" : \"SUM\",\n" +
+                    "      \"nested_filter\" : {\n" +
+                    "        \"bool\" : {\n" +
+                    "          \"should\" : [ {\n" +
+                    "            \"match\" : {\n" +
+                    "              \"info.name\" : {\n" +
+                    "                \"query\" : \"'java'\",\n" +
+                    "                \"type\" : \"boolean\"\n" +
+                    "              }\n" +
+                    "            }\n" +
+                    "          }, {\n" +
+                    "            \"term\" : {\n" +
+                    "              \"info.name\" : \"python\"\n" +
+                    "            }\n" +
+                    "          }, {\n" +
+                    "            \"match\" : {\n" +
+                    "              \"info.name\" : {\n" +
+                    "                \"query\" : \"Hadoop\",\n" +
+                    "                \"type\" : \"phrase\"\n" +
+                    "              }\n" +
+                    "            }\n" +
+                    "          } ]\n" +
+                    "        }\n" +
+                    "      },\n" +
+                    "      \"nested_path\" : \"info\"\n" +
+                    "    }\n" +
+                    "  }, {\n" +
+                    "    \"score\" : {\n" +
+                    "      \"order\" : \"desc\"\n" +
+                    "    }\n" +
+                    "  } ]\n" +
+                    "}";
+            Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void nestedSortTest2(){
+        String sql = "SELECT * FROM test order by nested(a,sum(a.b),a.c=matchQuery(\"百度和谷歌\"))";
+        String actual = exec(sql);
+        String expected = "{\n" +
+                "  \"from\" : 0,\n" +
+                "  \"size\" : 200,\n" +
+                "  \"sort\" : [ {\n" +
+                "    \"a.b\" : {\n" +
+                "      \"order\" : \"asc\",\n" +
+                "      \"missing\" : \"_first\",\n" +
+                "      \"mode\" : \"SUM\",\n" +
+                "      \"nested_filter\" : {\n" +
+                "        \"bool\" : {\n" +
+                "          \"should\" : {\n" +
+                "            \"match\" : {\n" +
+                "              \"a.c\" : {\n" +
+                "                \"query\" : \"'百度和谷歌'\",\n" +
+                "                \"type\" : \"boolean\"\n" +
+                "              }\n" +
+                "            }\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"nested_path\" : \"a\"\n" +
+                "    }\n" +
+                "  } ]\n" +
+                "}";
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void nestedSortTest3(){
+        String sql = "SELECT * FROM test order by nested(a,sum(a.b),a.c=term(\"百度和谷歌\"))";
+        String actual = exec(sql);
+        String expected = "{\n" +
+                "  \"from\" : 0,\n" +
+                "  \"size\" : 200,\n" +
+                "  \"sort\" : [ {\n" +
+                "    \"a.b\" : {\n" +
+                "      \"order\" : \"asc\",\n" +
+                "      \"missing\" : \"_first\",\n" +
+                "      \"mode\" : \"SUM\",\n" +
+                "      \"nested_filter\" : {\n" +
+                "        \"bool\" : {\n" +
+                "          \"should\" : {\n" +
+                "            \"term\" : {\n" +
+                "              \"a.c\" : \"百度和谷歌\"\n" +
+                "            }\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"nested_path\" : \"a\"\n" +
+                "    }\n" +
+                "  } ]\n" +
+                "}";
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void nestedSortTest4(){
+        String sql = "SELECT * FROM test order by nested(a,sum(a.b),a.c=\"百度和谷歌\")";
+        String actual = exec(sql);
+        String expected = "{\n" +
+                "  \"from\" : 0,\n" +
+                "  \"size\" : 200,\n" +
+                "  \"sort\" : [ {\n" +
+                "    \"a.b\" : {\n" +
+                "      \"order\" : \"asc\",\n" +
+                "      \"missing\" : \"_first\",\n" +
+                "      \"mode\" : \"SUM\",\n" +
+                "      \"nested_filter\" : {\n" +
+                "        \"bool\" : {\n" +
+                "          \"should\" : {\n" +
+                "            \"match\" : {\n" +
+                "              \"a.c\" : {\n" +
+                "                \"query\" : \"百度和谷歌\",\n" +
+                "                \"type\" : \"phrase\"\n" +
+                "              }\n" +
+                "            }\n" +
+                "          }\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"nested_path\" : \"a\"\n" +
+                "    }\n" +
+                "  } ]\n" +
+                "}";
+        Assert.assertEquals(expected,actual);
+    }
+
+    public static String exec(String sql){
+        String jsonExplanation = "";
         try {
             TransportClient client;
             client = TransportClient.builder().addPlugin(DeleteByQueryPlugin.class).build().addTransportAddress(getTransportAddress());
-            String sql = "select * from test where nested(info,info.name=term(\"java\") and info.name=term(\"python\") or info.name=term(\"Hadoop\")) order by nested(info,sum(info.age),info.name=matchQuery(\"java\") and info.name=term(\"python\") and info.name=\"Hadoop\") desc,score desc";
-            //String sql = "SELECT * FROM test order by nested(a,sum(a.b),a.c=matchQuery(\"百度和谷歌\"))";
-            //String sql = "SELECT * FROM test order by nested(a,sum(a.b),a.c=term(\"百度和谷歌\") and a.c=term(\"test\"))";
-            //String sql = "SELECT * FROM test order by nested(a,sum(a.b),a.c=\"百度和谷歌\")";
-            //String sql = "SELECT * FROM test where nested(calc_skill_blog,calc_skill_blog.keyword=\"java\")";
-            Long now = System.currentTimeMillis();
+            //Long now = System.currentTimeMillis();
             SearchDao searchDao = new SearchDao(client);
             QueryAction queryAction = searchDao.explain(sql);
             SqlElasticRequestBuilder xx = queryAction.explain();
-            String jsonExplanation = xx.explain();
-            //String jsonExplanation = queryAction.explain().explain();
-            System.out.println(System.currentTimeMillis() - now + " ms");
-            System.out.println(jsonExplanation);
+            jsonExplanation = xx.explain();
+            //System.out.println(System.currentTimeMillis() - now + " ms");
+            //System.out.println(jsonExplanation);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return  jsonExplanation;
     }
-
 
     public static InetSocketTransportAddress getTransportAddress() throws UnknownHostException {
 
