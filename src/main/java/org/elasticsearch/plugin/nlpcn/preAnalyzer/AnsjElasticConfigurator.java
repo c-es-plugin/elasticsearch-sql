ad35623259772d4c3401c9121a3db1ce41a37e6c
gitxxx <binsandy@qq.com>
2016-12-16 12:35:28 +0800

    modify es-sql init config

--- src/main/java/org/elasticsearch/plugin/nlpcn/preAnalyzer/AnsjElasticConfigurator.java
+++ src/main/java/org/elasticsearch/plugin/nlpcn/preAnalyzer/AnsjElasticConfigurator.java
@@ -17,18 +17,21 @@ public class AnsjElasticConfigurator {
 
     public static void init(Settings settings) {
         try {
-            ES_IP = settings.get("network.host");
+            ES_IP = settings.get("ansj.host");
             ES_PORT = settings.get("http.port");
-            logger.info("es 本地IP：" + ES_IP);
-            logger.info("es 端口："+ ES_PORT);
-            if (ES_IP==null || ES_IP.equals("")){
-                logger.error("本地IP获取失败");
+            if (ES_IP == null || ES_IP.equals("")) {
+                logger.error("ansj.host获取失败");
+            } else {
+                logger.info("ansj.host：" + ES_IP);
             }
-            if (ES_PORT==null || ES_PORT.equals("")){
-                logger.error("ES服务获取失败");
+            if (ES_PORT == null || ES_PORT.equals("")) {
+                ES_PORT = "9200";
+                logger.error("ES服务获取失败,使用默认端口:9200");
+            } else {
+                logger.info("http.port："+ ES_PORT);
             }
         } catch (Exception e) {
-            logger.error("本地IP和ES端口获取失败");
+            logger.error(e.getMessage());
         }
     }
 }
