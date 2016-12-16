b667d6b6ccbb9ad4c9c5c6ddf1abae629bbcf1ee
gitxxx <binsandy@qq.com>
2016-12-16 13:18:05 +0800

    modify init config

--- src/main/java/org/elasticsearch/plugin/nlpcn/preAnalyzer/AnsjElasticConfigurator.java
+++ src/main/java/org/elasticsearch/plugin/nlpcn/preAnalyzer/AnsjElasticConfigurator.java
@@ -17,16 +17,16 @@ public class AnsjElasticConfigurator {
 
     public static void init(Settings settings) {
         try {
-            ES_IP = settings.get("ansj.host");
+            ES_IP = settings.get("network.host");
             ES_PORT = settings.get("http.port");
             if (ES_IP == null || ES_IP.equals("")) {
-                logger.error("ansj.host获取失败");
+                logger.error("network.host获取失败");
             } else {
-                logger.info("ansj.host：" + ES_IP);
+                logger.info("network.host：" + ES_IP);
             }
             if (ES_PORT == null || ES_PORT.equals("")) {
                 ES_PORT = "9200";
-                logger.error("ES服务获取失败,使用默认端口:9200");
+                logger.error("http.port获取失败,使用默认端口:9200");
             } else {
                 logger.info("http.port："+ ES_PORT);
             }
